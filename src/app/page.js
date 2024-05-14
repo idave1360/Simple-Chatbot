3fd3439d75c1a64102a038d2ed7a2f705939215f
idave1360 <102980889+idave1360@users.noreply.github.com>
2024-05-14 11:18:27 +0900

    - 불필요한 파일 삭제 - 기본적인 챗봇 기능 구현

--- src/app/page.js
+++ src/app/page.js
@@ -109,7 +109,7 @@ export default function Home() {
           <div className="font-bold text-3xl flex text-center">
             <a
               className="ml-2 hover:opacity-50"
-              href="https://code-scaffold.vercel.app"
+              // href="https://code-scaffold.vercel.app"
             >
               A Simple Chatbot
             </a>
